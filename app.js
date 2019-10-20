1c981118acb0464ec32c3648427a3d102adf3405
root <root@userver.ibm.demo>
2019-10-20 20:29:26 +0000

    updated version to v3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Cloud Paks!\n v2");
+  res.send("Hello from Cloud Paks!\n v3");
 });
  
 module.exports.app = app;
